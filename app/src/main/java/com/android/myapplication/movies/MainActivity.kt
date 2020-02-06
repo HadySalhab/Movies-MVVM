d9a399036d32ae7e91f04ee402ce06250556c310
Hady Salhab <hadysalhab1@gmail.com>
2020-02-06 21:05:46 +1100

    fix: Delete BaseActivity

--- app/src/main/java/com/android/myapplication/movies/MainActivity.kt
+++ app/src/main/java/com/android/myapplication/movies/MainActivity.kt
@@ -12,7 +12,7 @@ import retrofit2.Callback
 import retrofit2.Response
 import java.io.IOException
 
-class MainActivity :BaseActivity(){
+class MainActivity :AppCompatActivity(){
     val movieApi:MoviesApi by inject()
 
     companion object {
@@ -22,38 +22,37 @@ class MainActivity :BaseActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        testRetrofitRequest()
     }
 
-    fun testRetrofitRequest(){
-        val responseCall = movieApi.getMovieDetail(id=157336)
-        responseCall.enqueue(object:Callback<MovieDetailsResponse>{
-            override fun onFailure(call: Call<MovieDetailsResponse>, t: Throwable) {
-                Log.d(TAG, "onFailure: server response failed ${t.message}}")
-            }
-
-            override fun onResponse(
-                call: Call<MovieDetailsResponse>,
-                response: Response<MovieDetailsResponse>
-            ) {
-                Log.d(TAG, "onResponse: server response: ${response} ")
-                if(response.code() == 200){
-                    Log.d(TAG,"onResponse: ${response.body()}")
-                    val moviesTitle = response.body()?.title
-                    moviesTitle?.let {
-                        moviesTitle.forEach { moviesTitle->
-                            Log.d(TAG,"movie: ${moviesTitle}")
-                        }
-                    }
-                } else{
-                    try {
-                        Log.d(TAG,"onResponse: ${response.errorBody()}")
-                    }catch (e:IOException){
-                        e.printStackTrace()
-                    }
-                }
-            }
-
-        })
-    }
+//    fun testRetrofitRequest(){
+//        val responseCall = movieApi.getMovieDetail(id=157336)
+//        responseCall.enqueue(object:Callback<MovieDetailsResponse>{
+//            override fun onFailure(call: Call<MovieDetailsResponse>, t: Throwable) {
+//                Log.d(TAG, "onFailure: server response failed ${t.message}}")
+//            }
+//
+//            override fun onResponse(
+//                call: Call<MovieDetailsResponse>,
+//                response: Response<MovieDetailsResponse>
+//            ) {
+//                Log.d(TAG, "onResponse: server response: ${response} ")
+//                if(response.code() == 200){
+//                    Log.d(TAG,"onResponse: ${response.body()}")
+//                    val moviesTitle = response.body()?.title
+//                    moviesTitle?.let {
+//                        moviesTitle.forEach { moviesTitle->
+//                            Log.d(TAG,"movie: ${moviesTitle}")
+//                        }
+//                    }
+//                } else{
+//                    try {
+//                        Log.d(TAG,"onResponse: ${response.errorBody()}")
+//                    }catch (e:IOException){
+//                        e.printStackTrace()
+//                    }
+//                }
+//            }
+//
+//        })
+//    }
 }
