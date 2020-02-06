1900e59d95a2a43a048c5f7d97943df617f439ca
Hady Salhab <hadysalhab1@gmail.com>
2020-02-07 01:23:07 +1100

    feat: Display Loading Animation When query performed

--- app/src/main/java/com/android/myapplication/movies/ui/MovieListViewModel.kt
+++ app/src/main/java/com/android/myapplication/movies/ui/MovieListViewModel.kt
@@ -6,15 +6,16 @@ import com.android.myapplication.movies.repository.MoviesRepository
 import com.android.myapplication.movies.util.Categories
 import com.android.myapplication.popularmovies.api.model.Movie
 
-class MovieListViewModel(private val repository: MoviesRepository) :ViewModel(){
+class MovieListViewModel(private val repository: MoviesRepository) : ViewModel() {
 
-    val movieList:LiveData<List<Movie>> = repository.movieList
+    var isPerformingQuery = false
+    val movieList: LiveData<List<Movie>> = repository.movieList
 
-    fun getMovies(pageNumber:Int,sortBy: Categories){
-        repository.getMovies(pageNumber,sortBy)
-    }
-    fun searchMovies(pageNumber:Int,query:String){
-        repository.searchMovies(pageNumber,query)
+    fun getMovies(pageNumber: Int, sortBy: Categories) {
+        repository.getMovies(pageNumber, sortBy)
     }
 
+    fun searchMovies(pageNumber: Int, query: String) {
+        repository.searchMovies(pageNumber, query)
+    }
 }
