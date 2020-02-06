0a5cd3d7accb7a628d6f5334580d8817bba5e3c7
Hady Salhab <hadysalhab1@gmail.com>
2020-02-06 17:26:51 +1100

    chore: Updating Gradle

--- app/src/main/java/com/android/myapplication/movies/MainActivity.kt
+++ app/src/main/java/com/android/myapplication/movies/MainActivity.kt
@@ -9,7 +9,6 @@ import org.koin.android.ext.android.inject
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import timber.log.Timber
 import java.io.IOException
 
 class MainActivity :BaseActivity(){
