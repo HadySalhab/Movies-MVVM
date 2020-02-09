64bea9afdb3b00bcdd387d622690705d1021b5ff
Hady Salhab <hadysalhab1@gmail.com>
2020-02-09 20:09:45 +1100

    feat: Add Generic ApiResponse Class

---
+++ app/src/main/java/com/android/myapplication/movies/api/responses/ApiResponse.kt
@@ -0,0 +1,40 @@
+package com.android.myapplication.movies.api.responses
+
+import retrofit2.Response
+
+sealed class ApiResponse<T> {
+    companion object {
+        fun <T> create(error: Throwable): ApiResponse<T> {
+            return ApiErrorResponse(error.message ?: "Unknown Error\nCheck Network Connection")
+        }
+
+        fun <T> create(response: Response<T>): ApiResponse<T> {
+            return if (response.isSuccessful) {
+                val body = response.body()
+                if (body == null || response.code() == 204){ //204 means empty Response
+                    ApiEmptyResponse()
+                } else {
+                    ApiSuccessResponse(
+                        body = body
+                    )
+                }
+            } else {
+                val msg = response.errorBody()?.string()
+                val errorMsg = if (msg.isNullOrEmpty()) {
+                    response.message()
+                } else {
+                    msg
+                }
+                ApiErrorResponse(errorMsg ?: "Unknown Error\n" +
+                "Check Network Connection")
+            }
+        }
+    }
+}
+
+/**
+ * separate class for HTTP 204 responses so that we can make ApiSuccessResponse's body non-null.
+ */
+class ApiEmptyResponse<T> : ApiResponse<T>()
+data class ApiSuccessResponse<T>(val body: T) : ApiResponse<T>()
+data class ApiErrorResponse<T>(val errorMessage: String) : ApiResponse<T>()
